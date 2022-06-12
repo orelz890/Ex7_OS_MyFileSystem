21a25424f191a72e5dd8a13fc1a13f59ffc98382
orel <orelzx13@gmail.com>
2022-06-12 18:49:27 +0300

    first

---
+++ my_fc.cpp
@@ -0,0 +1,143 @@
+#include "my_fs.hpp"
+
+
+// my_file myopenfile[MAX_FILES];
+// super_block sb;
+// inode *inodes;
+// disk_block *dbs;
+
+inode::inode()
+{
+    this->size = -1;
+    this->first_block = -1;
+}// inode
+
+
+disk_block::disk_block()
+{
+    this->next_block_num = -1;
+}// disk_block
+
+
+my_dirent::my_dirent()
+{
+    for (int i = 0; i < DIR_SIZE; i++)
+    {
+        this->fds[i] = -1;
+    } 
+}// my_dirent
+
+
+int find_empty_block()
+{
+    
+    for (int i = 0; i < sb.num_blocks; i++)
+    {
+        if (dbs[i].next_block_num == -1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}// find_empty_block
+
+
+int find_empty_inode()
+{
+    for (int i = 0; i < sb.num_inodes; i++)
+    {
+        if (inodes[i].first_block == -1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}// find_empty_inode
+
+
+void shorten_file(int bn)
+{
+    int nn = dbs[bn].next_block_num;
+    if (dbs[bn].next_block_num >= 0)
+    {
+        shorten_file(nn);
+    }
+    dbs[bn].next_block_num = -1;
+}
+
+
+void set_filesize(int filenum, int size)
+{
+    int temp = size + BLOCK_SIZE - 1;
+    int num = temp / BLOCK_SIZE;
+    int bn = inodes[filenum].first_block;
+    num--;
+    // grow if necessary
+    while (num > 0)
+    {
+        // check next block number
+        int next_num = dbs[bn].next_block_num;
+        if (next_num == -2)
+        {
+            int empty = find_empty_block();
+            dbs[bn].next_block_num = empty;
+            dbs[empty].next_block_num = -2;
+        }
+        bn = dbs[bn].next_block_num;
+        num--;
+    }
+    // short if necessary
+    shorten_file(bn);
+    dbs[bn].next_block_num = -2;
+}
+
+
+int alloc_file(std::string name, int size)
+{
+    int empty_inode = find_empty_inode();
+    int first_block = find_empty_block();
+    if (empty_inode != -1 && first_block != -1)
+    {   
+        inodes[empty_inode].size = size;
+        inodes[empty_inode].first_block = first_block;
+        dbs[first_block].next_block_num = -2;
+        inodes[empty_inode].name = name;
+        set_filesize(empty_inode, size);
+    }
+    else
+    {
+        empty_inode == -1? printf("Dont have enough inodes!\n") : printf("Dont have enough blocks!\n");
+        exit(1);
+    }
+    return empty_inode;
+}
+
+
+// Initialize new filesystem
+void mymkfs(int size)
+{
+    int inode_size = sizeof(inode);
+    sb.num_inodes = size / (10*inode_size);
+    inodes = new inode[sb.num_inodes];    
+    int block_size = sizeof(disk_block); 
+    sb.num_blocks = (size - (sb.num_inodes * inode_size))/block_size;
+    dbs = new disk_block[sb.num_blocks];
+    // alloc_file("init", sizeof(mydirent));
+}
+
+
+int mymount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data)
+{
+    
+}
+int myopen(const char *pathname, int flags);
+int myclose(int myfd);
+size_t myread(int myfd, void *buf, size_t count);
+size_t mywrite(int myfd, const void *buf, size_t count);
+off_t mylseek(int myfd, off_t offset, int whence);
+myDIR *myopendir(const char *name);
+mydirent *myreaddir(myDIR *dirp);
+int myclosedir(myDIR *dirp);
+
+
+void print_fs(); // print out info about file system
---
+++ my_fs.hpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+
+#define BLOCK_SIZE 512
+#define MAX_FILES 10000
+#define DIR_SIZE 10
+
+
+typedef struct super_block
+{
+    int num_inodes;
+    int num_blocks;
+    int size_blocks;
+}super_block;
+
+typedef struct inode
+{
+    int size;
+    int first_block;
+    std::string name;
+
+    inode();
+}inode;
+
+
+typedef struct disk_block
+{
+    int next_block_num;
+    char data[BLOCK_SIZE];
+
+    disk_block();   
+}disk_block;
+
+
+typedef struct my_file 
+{
+    int fd;
+    int pos; 
+}my_file;
+
+
+typedef struct my_dir 
+{
+    int n;
+    std::string name;
+} myDIR;
+
+
+typedef struct my_dirent { 
+    int size;
+    int fds[DIR_SIZE];
+    std::string name;
+
+    my_dirent();
+
+}mydirent;
+
+
+my_file myopenfile[MAX_FILES];
+super_block sb;
+inode *inodes;
+disk_block *dbs;
+
+void mymkfs(int size);
+int mymount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data);
+int myopen(const char *pathname, int flags);
+int myclose(int myfd);
+size_t myread(int myfd, void *buf, size_t count);
+size_t mywrite(int myfd, const void *buf, size_t count);
+off_t mylseek(int myfd, off_t offset, int whence);
+myDIR *myopendir(const char *name);
+mydirent *myreaddir(myDIR *dirp);
+int myclosedir(myDIR *dirp);
+
+
+void print_fs(); // print out info about file system
